9e4bedf8c8047fe556d56ba20363a0a582264e4e
TechBhaskar1 <81464944+TechBhaskar1@users.noreply.github.com>
2021-07-19 08:15:24 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+console.log("My first console project");
+var first_name="Bhaskar";
+var last_name="krishnan";
+console.log(first_name);
+console.log(last_name);
+console.log(first_name.concat(last_name));
+
+var number1=10;
+var number2=20;
+console.log(number1+number2);
+console.log(number2-number1);
+console.log(number1*number2);
+console.log(number2/number1);
